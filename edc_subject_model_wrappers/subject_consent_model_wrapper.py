9d49f702e8affbc9286f8b8e3b994c7cf3e3944c
erikvw <ew2789@gmail.com>
2019-03-28 17:19:00 -0500

    default to settings attr for models (consent, visit, requisition)

--- edc_subject_model_wrappers/subject_consent_model_wrapper.py
+++ edc_subject_model_wrappers/subject_consent_model_wrapper.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from edc_model_wrapper import ModelWrapper
 
 
 class SubjectConsentModelWrapper(ModelWrapper):
 
-    model = None  # e.g. myapp.subjectconsent
+    model = settings.SUBJECT_CONSENT_MODEL
     next_url_name = "subject_dashboard_url"
     next_url_attrs = ["subject_identifier"]
     querystring_attrs = [
--- edc_subject_model_wrappers/tests/tests/test_subject_visit_model_wrapper.py
+++ edc_subject_model_wrappers/tests/tests/test_subject_visit_model_wrapper.py
@@ -1,26 +1,37 @@
 from django.test import TestCase, tag
+from edc_dashboard.url_names import url_names
+from edc_subject_model_wrappers import SubjectVisitModelWrapper
 from edc_utils import get_utcnow
 from edc_visit_schedule import site_visit_schedules
-from edc_subject_model_wrappers import SubjectVisitModelWrapper
 
 from ..models import SubjectVisit, Appointment
 from ..visit_schedule import visit_schedule1
 
 
-class MySubjectVisitModelWrapper(SubjectVisitModelWrapper):
-
-    model = "edc_subject_model_wrappers.subjectvisit"
-
-
 class TestModelWrapper(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        url_names.register(
+            "subject_dashboard_url",
+            "subject_dashboard_url",
+            "edc_subject_model_wrapper",
+        )
+        return super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        url_names.registry.pop("subject_dashboard_url")
+        super().tearDownClass()
+
     def setUp(self):
         self.subject_identifier = "12345"
         site_visit_schedules._registry = {}
         site_visit_schedules.register(visit_schedule=visit_schedule1)
 
+    @tag("1")
     def test_(self):
         model_obj = SubjectVisit()
-        wrapper = MySubjectVisitModelWrapper(model_obj=model_obj)
+        wrapper = SubjectVisitModelWrapper(model_obj=model_obj)
         self.assertEqual(wrapper.model, "edc_subject_model_wrappers.subjectvisit")
         self.assertEqual(wrapper.model_cls, SubjectVisit)
 
@@ -34,5 +45,5 @@ class TestModelWrapper(TestCase):
             visit_code="1000",
         )
         subject_visit = SubjectVisit.objects.create(appointment=appointment)
-        wrapper = MySubjectVisitModelWrapper(model_obj=subject_visit)
+        wrapper = SubjectVisitModelWrapper(model_obj=subject_visit)
         self.assertEqual(str(appointment.id), wrapper.appointment)
